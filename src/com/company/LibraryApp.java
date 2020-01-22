0f7f6dbe4f281c0775f5b485339a229e63989cbe
Mattias Englund <mattias.a.englund@gmail.com>
2020-01-22 15:32:54 +0100

    Got distracted on what I was supposed to do and did alot of shit I shouldn't

--- src/com/company/LibraryApp.java
+++ src/com/company/LibraryApp.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import javax.management.relation.Role;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -23,37 +24,129 @@ public class LibraryApp {
     private String description;
     private int noOfPages;
     private boolean isAvailable;
-
-    public void librarianMenu(int choice){
-        System.out.println("Welcome Librarian what would you like to do:");
-        System.out.println("1. Add a book to the library.");
-        choice = scanner.nextInt();
-        if (choice == 1){
-            scanner.nextLine();
-            System.out.println("Enter book title: ");
-            title = scanner.nextLine();
-            System.out.println("Enter book author: ");
-            author = scanner.nextLine();
-            System.out.println("Enter a short description: ");
-            description = scanner.nextLine();
-            System.out.println("Enter number of pages: ");
-            noOfPages = scanner.nextInt();
-
-            addBookToLibrary(new Book(title, author, description ,noOfPages, true));
+    /*
+    public void libraryLogin (){
+        System.out.println("Welcome to Library app of the century");
+        System.out.println("Enter login name: ");
+        userName = scanner.nextLine();
+        System.out.println("Enter your pin: ");
+        userPin = scanner.nextInt();
+        try{
+            isLibrarian(userName, userPin);
+        } catch (Exception e){
+            System.out.println("Not librarian");
         }
-        if (choice == 2){
-            showBooksInLibrary();
+        try{
+            isMember(userName, userPin);
+        } catch (Exception e){
+            System.out.println("Not a member");
         }
     }
 
-    public void addMember(){
+    public Roles isLibrarian(String name, int pin){
+        librarians = (ArrayList<Librarian>)(readObject("librarians"));
+        for (int i = 0; i < librarians.size(); i++){
+            if (name.equalsIgnoreCase(librarians.get(i).getName())){
+                if(pin == librarians.get(i).getPin()){
+                    System.out.println("Successful login!");
+                    return librarians.get(i).getRole();
+                } else {
+                    System.out.println("Wrong pin");
+                }
+            }
+            else {
+                System.out.println("No user with that name.");
+                return null;
+            }
+        }
+        return null;
+    }
 
+    public Roles isMember(String name, int pin){
+        members = (ArrayList<Member>)(readObject("members"));
+        for (int i = 0; i < members.size(); i++){
+            if (name.equalsIgnoreCase(members.get(i).getName())){
+                if(pin == members.get(i).getPin()){
+                    System.out.println("Successful login!");
+                    return members.get(i).getRole();
+                } else {
+                    System.out.println("Wrong pin");
+                }
+            }
+            else {
+                System.out.println("Np user with that name.");
+                return null;
+            }
+        }
+        return null;
+    }
+*/
+
+    public void mainMenu(){
+        System.out.println("Welcome member, what would you like to do: ");
+        //System.out.println("1. Add a book to the library. ");
+        System.out.println("1. Show available books.");
+        System.out.println("2. Find out more about a book.");
+        //System.out.println("3. Add Member. ");
+        //System.out.println("4. Show Members. ");
+        choice = scanner.nextInt();
+        /*if (choice == 1){
+                scanner.nextLine();
+                System.out.println("Enter book title: ");
+                title = scanner.nextLine();
+                System.out.println("Enter book author: ");
+                author = scanner.nextLine();
+                System.out.println("Enter a short description: ");
+                description = scanner.nextLine();
+                System.out.println("Enter number of pages: ");
+                noOfPages = scanner.nextInt();
+
+                addBookToLibrary(new Book(title, author, description ,noOfPages, true));
+                System.out.println("Would you like to add another? y/n");
+                String answer = scanner.nextLine();
+
+        }*/
+        if (choice == 1){
+            showBooksInLibrary();
+        }
+        if (choice == 2){
+            findOutMoreAboutBook();
+        }
+        if(choice == 3){
+
+        }
+    }
+    public void findOutMoreAboutBook(){
+        books = (ArrayList<Book>)(readObject("books"));
+        scanner.nextLine();
+        System.out.println("Enter which title you would like to find out more about: ");
+        title = scanner.nextLine();
+        for(int i = 0; i < books.size(); i++){
+            if (title.equals(books.get(i).getTitle())){
+                System.out.println(books.get(i));
+            } else {
+
+            }
+        }
+
+    }
+
+    public void addMember(Member member){
+        members.add(member);
+        FileUtils.writeObject(members, "members");
+    }
+    public void showMembers(){
+        members = (ArrayList<Member>)(readObject("members"));
+        System.out.println(members);
     }
     public void addBookToLibrary(Book book){
         books.add(book);
         FileUtils.writeObject(books, "books");
     }
     public void showBooksInLibrary(){
-        System.out.println(readObject("books.ser"));
+       books = (ArrayList<Book>)(readObject("books"));
+        for (Book book : books) {
+            System.out.println(book.getTitle());
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    LibraryApp libraryApp = new LibraryApp();
 
-	    libraryApp.librarianMenu(1);
+        LibraryApp libraryApp = new LibraryApp();
+        libraryApp.addMember(new Member("Johan", Roles.MEMBER, 1234));
+        libraryApp.addBookToLibrary(new Book("Catcher", "Sven", "Book about a catcher", 1337, true));
+        libraryApp.addBookToLibrary(new Book("Bert", "Some dude", "Book about a Bert", 1234, false));
+        libraryApp.addBookToLibrary(new Book("Computer Science", "Benny", "Book about a computer science", 137, true));
+        libraryApp.mainMenu();
+
     }
 }
